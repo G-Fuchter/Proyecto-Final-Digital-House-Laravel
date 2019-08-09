575948f1c07d5aac1fd330c4a3f768d9e1175f63
Guillermo Fuchter <gpfuchter@gmail.com>
2019-08-09 18:05:53 -0300

    First commit

---
+++ Nam/resources/views/includes/footer.blade.php
@@ -0,0 +1,48 @@
+   
+    </main>
+     <!-- /MAIN -->
+    <footer class="container py-5">
+        <div class="row">
+            <div class="col-12 col-md">
+                <a class="footer-brand" href="index.html">¡Ñam!</a>
+                <small class="d-block mb-3 text-muted">&copy; 2017-2019</small>
+            </div>
+            <div class="col-6 col-md">
+                <h5 class="gill accent">Nuestros packs</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Veggie</a></li>
+                    <li><a class="text-muted" href="#">Fit</a></li>
+                    <li><a class="text-muted" href="#">Vegan</a></li>
+                    <li><a class="text-muted" href="#">Random!</a></li>
+                    <li><a class="text-muted" href="#">Postres</a></li>
+                    <li><a class="text-muted" href="#">Goloso</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5 class="gill accent">Comida riquisima</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Arma tu menú</a></li>
+                    <li><a class="text-muted" href="#">Nuestros chef</a></li>
+                    <li><a class="text-muted" href="#">Materia prima</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5 class="gill accent">Somos ÑAM</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="#">Equipo</a></li>
+                    <li><a class="text-muted" href="#">Faqs</a></li>
+                    <li><a class="text-muted" href="#">Contacto</a></li>
+                    <li><a class="text-muted" href="#">Deja tu testimonio</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')
+    </script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ Nam/resources/views/includes/header.blade.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Victoria, Gabriela y Guillermo">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>¡Ñam!</title>
+
+    <link rel="canonical" href="#">
+
+    <!-- Bootstrap core CSS -->
+    <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="css/theme.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+
+
+</head>
+
+<body>
+    <header class="fixed-top  accent_bg main_header">
+        <div class="container">
+            <div class="top_bar row d-flex justify-content-between align-items-center pt-2">
+                <ul class="social  d-flex flex-row justify-content-between align-items-center list-unstyled">
+                    <li class="px-2">
+                        <a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                    </li>
+                    <li class="px-2">
+                        <a href="#" target="_blank"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li class="px-2">
+                        <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+                    </li>
+                </ul>
+                <nav class="user_nav">
+                    <ul class="list-unstyled d-flex flex-row justify-content-between align-items-center list-unstyled">
+                        <li class="px-2">
+                            <a href=" login.php " class="small white ">Login</a>
+                        </li>
+                        <li class="px-2">
+                            <a href="registro.php " class="small white ">Registro</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="row ">
+                <nav class="navbar navbar-expand-md row d-flex justify-content-between w-100 ">
+                    <a class="navbar-brand d-sm-none " href="index.php ">¡Ñam!</a>
+                    <button class="navbar-toggler accent_border " type="button " data-toggle="collapse " data-target="#toogleNavbar " aria-controls="toogleNavbar " aria-expanded="false " aria-label="Toggle navigation "><i class="fas fa-bars "></i></button>
+
+                    <div class="collapse navbar-collapse ml-auto justify-content-center align-content-center align-items-center " id="toogleNavbar ">
+                        <ul class="navbar-nav ">
+                            <li class="nav-item ">
+                                <a class="nav-link dark kappa-bold text-uppercase " href="# ">¡Comida rica!</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link dark kappa-bold text-uppercase " href="# ">Packs</a>
+                            </li>
+                            <li class="nav-item d-sm-block d-xs-none ">
+                                <a class="navbar-brand " href="index.php ">¡Ñam!</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link dark kappa-bold text-uppercase " href="# ">Comprar</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link dark kappa-bold text-uppercase " href="# ">Faqs</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link dark kappa-bold text-uppercase " href="# "><i class="fas fa-shopping-cart "></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+    </header>
+    <main role="main">
